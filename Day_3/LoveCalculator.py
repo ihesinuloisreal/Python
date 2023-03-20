2b5ac99b0cbb1203c7a665b01842e072ffcd91c3
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-03-20 16:28:46 -0700

    optimized

--- Day_3/LoveCalculator.py
+++ Day_3/LoveCalculator.py
@@ -1,25 +1,17 @@
 Your_Name = input("What is your name? ")
 Your_crush_name = input("What is your crush/admirer name? ")
+
+possible = Your_crush_name + Your_Name
 a = 0
 b = 0
 
-a += Your_Name.lower().count("t")
-a += Your_Name.lower().count("r")
-a += Your_Name.lower().count("u")
-a += Your_Name.lower().count("e")
+a += possible.lower().count("t")
+a += possible.lower().count("r")
+a += possible.lower().count("u")
+a += possible.lower().count("e")
 
-a += Your_crush_name.lower().count("t")
-a += Your_crush_name.lower().count("r")
-a += Your_crush_name.lower().count("u")
-a += Your_crush_name.lower().count("e")
-
-b += Your_Name.lower().count("l")
-b += Your_Name.lower().count("o")
-b += Your_Name.lower().count("v")
-b += Your_Name.lower().count("e")
-
-b += Your_crush_name.lower().count("l")
-b += Your_crush_name.lower().count("o")
-b += Your_crush_name.lower().count("v")
-b += Your_crush_name.lower().count("e")
+b += possible.lower().count("l")
+b += possible.lower().count("o")
+b += possible.lower().count("v")
+b += possible.lower().count("e")
 print(f"{a}{b}")
