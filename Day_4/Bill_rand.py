c2ef41d5f825028cd5307b8f5fdaef7a077d51c1
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-03-25 05:27:55 -0700

    bill update

--- Day_4/Bill_rand.py
+++ Day_4/Bill_rand.py
@@ -1,6 +1,6 @@
 import random
-name = input("Type in a word. ")
+name = input("Type in the names of persons to play. ")
 name_ = name.split()
 out = random.randint(0, name_.__len__()-1)
 
-print(f"The number position is {out} and the value is {name_[out]}")
+print(f"The number position is {out} and the Person to pay the bills is {name_[out]}")
