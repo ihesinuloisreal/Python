18693f8f8f4c35f30a951436aaeb266171242f5b
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-06 14:50:27 -0700

    loop break

--- Day_7/Hangman.py
+++ Day_7/Hangman.py
@@ -9,14 +9,17 @@ word_length = len(choosen_word)
 for _ in range(word_length):
     display += "_"
 print(display)
+end_of_game = False
 
+while not end_of_game :
+    guess = input("Guess a number: ").lower()
 
-guess = input("Guess a number: ").lower()
-
-for position in range(word_length):
-    leters = choosen_word[position]
-    if leters == guess:
-        display[position] = leters
-    
-
-print(display)
+    for position in range(word_length):
+        leters = choosen_word[position]
+        if leters == guess:
+            display[position] = leters
+        
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
