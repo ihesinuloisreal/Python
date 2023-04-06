371eab6869208aa9d839173c3bcfa1a2d1d6b4ea
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-06 14:28:55 -0700

    Editing field positions in a list

--- Day_7/Hangman.py
+++ Day_7/Hangman.py
@@ -3,10 +3,20 @@ word_list = ['word', 'home', 'frank']
 import random
 
 choosen_word = random.choice(word_list)
+print(choosen_word)
+display = []
+word_length = len(choosen_word)
+for _ in range(word_length):
+    display += "_"
+print(display)
+
+
 guess = input("Guess a number: ").lower()
 
-for leters in choosen_word:
+for position in range(word_length):
+    leters = choosen_word[position]
     if leters == guess:
-        print("Right")
-    else: 
-        print("Wrong")
+        display[position] = leters
+    
+
+print(display)
