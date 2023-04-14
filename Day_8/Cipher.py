ea322ccf744b55059d90d52abe9b3a33686d7486
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-14 15:12:22 -0700

    Cypher

--- Day_8/Cipher.py
+++ Day_8/Cipher.py
@@ -1,14 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 def encrypt(text, shift):
-    
-    # texts = [*text]
     encode=""
     for i in text:
-        print(f"{i} {alphabet.index(i)+1}")
         position = f"{alphabet.index(i) + shift}"
         new = (int(position))
         encode += alphabet[new]
@@ -16,16 +11,25 @@ def encrypt(text, shift):
 
     
 
-# def decrypt():
-#     text = input("Type your message:\n").lower()
-#     shift = int(input("Type the shift number:\n"))
-#     texts = [*text]
-#     decode=[]
-#     for j in encode:
-#         position = f"{alphabet.index(j) - shift}"
-#         new = (int(position))
-#         decode.append((alphabet[new]))
-#     print(decode)
+def decrypt(text, shift):
+    decode=""
+    for j in text:
+        position = f"{alphabet.index(j) - shift}"
+        new = (int(position))
+        decode+=alphabet[new]
+    print(decode)
 
+res = "yes"
 
-encrypt(text=text, shift=shift) 
+while res == "yes" :
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if direction == "encode" :
+        encrypt(text=text, shift=shift)
+    elif direction == "decode" :
+        decrypt(text=text, shift=shift)
+    else:
+        print("invalide input")
+
+    res = input("Would you want to continue: yes or no!").lower()
