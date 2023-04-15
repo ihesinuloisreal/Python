cdfae69ce91e00a66f585cde34f9f441bee7769f
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-15 15:32:18 -0700

    cipher update

--- Day_8/Cipher.py
+++ Day_8/Cipher.py
@@ -1,35 +1,36 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
 
 
-def encrypt(text, shift):
-    encode=""
-    for i in text:
-        position = f"{alphabet.index(i) + shift}"
-        new = (int(position))
-        encode += alphabet[new]
-    print(encode)
 
-    
 
-def decrypt(text, shift):
-    decode=""
-    for j in text:
-        position = f"{alphabet.index(j) - shift}"
-        new = (int(position))
-        decode+=alphabet[new]
-    print(decode)
+def ceasar(text, shift, direction):
+    res = True
+    while res == True :
+        final_text=""
+        if direction == "decode":
+            shift *= -1 
+        for i in text:
+            if i in alphabet:
+                position = alphabet.index(i)
+                new_position = position + shift
+                new = (int(new_position))
+                final_text += alphabet[new]
+            else:
+                final_text += i
+        print(final_text)
+        result = input("Do you want to continue? yes or no").lower()
+        if result == "no":
+            res = False
+            print("Good bye")
 
-res = "yes"
+from art import art
 
-while res == "yes" :
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    if direction == "encode" :
-        encrypt(text=text, shift=shift)
-    elif direction == "decode" :
-        decrypt(text=text, shift=shift)
-    else:
-        print("invalide input")
+print(art)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-    res = input("Would you want to continue: yes or no!").lower()
+shift %= 26
+
+
+ceasar(text, shift, direction)
