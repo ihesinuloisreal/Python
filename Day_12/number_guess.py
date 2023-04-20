bc58d8e09c9dd49f543f37a2548e10f9d0ff4cba
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-19 19:47:34 -0700

    number game

--- Day_12/number_guess.py
+++ Day_12/number_guess.py
@@ -1,32 +1,48 @@
 import random
+
+CORRECT = True
 # TODO 
-# WRITE A FUNCTION THAT WILL RANDOMLY GUESS NUMBERS WITHIN A RANGE
+# RANDOMLY GUESS NUMBERS WITHIN A RANGE AND SAVE TO A VARIABLE
 # WRITE A FUNCTION THAT WILL CHECK THE USER INPUT WITH THE GUESSED NUMBER
 # COLLECT INPUT FROM USERS
 number = value = random.randint(1, 100)
 
-def check_numbers(user_number, number):
+def check_numbers(user_number, number, turns):
     print(number)
     if number == user_number :
         global CORRECT
         CORRECT = False
         return "You win"
     elif number > user_number:
-        return "To Low"
+        print("Too Low")
+        return turns - 1
     else:
-        return "Too High"
+        print("Too High")
+        return turns - 1
 
-print("Welcome to the Guessing game")
-difficulty = 0
-if input("Select difficulty 'Easy' or 'Hard' ").lower() == "easy":
-    difficulty = 9
-elif input("Select difficulty 'Easy' or 'Hard' ").lower() == "hard":
-    difficulty = 4
-else:
-    print("Invalid Input")
- 
-CORRECT = True
-while CORRECT == True and difficulty > 0:
-    user_number = int(input("Guess a number: "))
-    output = check_numbers(user_number,number)
-    print(output)
+def level():
+    if input("Choose difficulty. Type 'Easy' or 'Hard': ").lower() == "easy":
+        return 10
+    else:
+        return 5
+
+def game():
+    print("Welcome to the Number Guessing game")
+    print("I am thinking of a number between 1 to 100")
+
+    turns = level()
+    print(f"You have {turns} times left to guess")
+
+    user_number = 0
+    while user_number != number:
+        user_number = int(input("Guess a number: "))
+        turns = check_numbers(user_number,number, turns)
+        # print(output)
+        print(f"You have {turns} times left to guess")
+        if turns == 0:
+            print("You run out of guess. You Lose")
+            return
+        elif user_number != number:
+            print("Guess again")
+
+game()
