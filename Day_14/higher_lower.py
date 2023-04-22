11ed6d5289d5f3ad193ee3f9d6293bb4c63efb6c
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-22 15:58:59 -0700

    22

--- Day_14/higher_lower.py
+++ Day_14/higher_lower.py
@@ -7,50 +7,53 @@ score = 0
 
 def generate():
     return random.choice(data)
-
-A = generate()
-B = generate()
+A = 0
+B = 0
+for _ in range(1):
+    A = generate()
+    B = generate()
 def check(responce):
     global score
-    if responce == "a":
-        value = str(A["follower_count"]) > str(B["follower_count"])
-        score += 1
-        return value
-    else:
-        value = str(A["follower_count"]) < str(B["follower_count"])
-        score += 1
-        return value
-
-def run():
     global A
     global B
+    if responce == "a":
+        if A["follower_count"] > B["follower_count"]:
+            A = A
+            return True
+        else:
+            return False
+        # return score + 1
+    elif responce == "b":
+        if B["follower_count"] > A["follower_count"]:
+            A = B
+            return True
+        else:
+            return False
+        # return score + 1
+    else:
+        print("Invalid input")
+
+
+def run():
     name = A["name"]
     print(f"{name}")
     print(vs)
     print(B["name"])
     print(A["follower_count"])
     print(B["follower_count"])
-    
-run()
-# responce = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+
 res = True
 while res == True:
     run()
     responce = input("Who has more followers? Type 'A' or 'B': ").lower()
+    print(check(responce))
+    if check(responce) == True:
+        B = generate()
+    # elif check(responce) != True:
+    #     res = False
 
-
-    # print(responce)
-    if check(responce) != True:
-        res = False
-    #     A["follower_count"] = B["follower_count"]
-    #     B = generate()["follower_count"]
-    #     print(B)
-    # print(score)
-answer = check(responce)
-print(answer)
-
-run()
-
+#     # print(responce)
 # print(score)
 
 # print(generate())
