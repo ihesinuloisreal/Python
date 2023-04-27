8d7aef1db3cf480edc827ba4f0fb76ee91558158
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-27 15:34:16 -0700

    coffee

--- Day_16/main.py
+++ Day_16/main.py
@@ -8,9 +8,13 @@
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-print(table)
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
