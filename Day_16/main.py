53ae678eba5463711874ccdff2a79ab79052e047
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-04-28 16:52:04 -0700

    coffee

--- Day_16/main.py
+++ Day_16/main.py
@@ -20,9 +20,14 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 main_menu = Menu()
+# main_menu_item = MenuItem(name, water, milk, coffee, cost)
 coffee_m = CoffeeMaker()
-money = MoneyMachine()
-
+money_machine = MoneyMachine()
 
 
 coffee_m.report()
+money_machine.report()
+# main_menu.get_items()
+# main_menu.Name = input("Please Choose your coffee flavor? ")
+# money.process_coins()
+# money.make_payment(cost)
